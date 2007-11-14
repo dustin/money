d0258fe66223001198e2caa27d1b535ff026b7af
Dustin Sallings <dustin@spy.net>
2007-11-13 23:47:30 -0800

    Account list as links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def amt_class(amt)
+    rv='positive'
+    if amt < 0
+      rv='negative'
+    end
+    rv
+  end
+
+  def currency_span(amt)
+    "<span class=\"#{amt_class(amt)}\">#{number_to_currency(amt)}</span>"
+  end
+
+	
+
 end
