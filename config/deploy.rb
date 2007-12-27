6f0db5dcdfb632b164172fab0e356c9041142eb3
Dustin Sallings <dustin@spy.net>
2007-12-26 23:10:24 -0800

    Moved deployment location.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :runner, 'www'
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/data/web/rails/cap/#{application}"
+set :deploy_to, "/data/web/rails/#{application}"
 
 set :scm, :mercurial
 
