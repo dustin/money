3b3e79fd32aa30bf8bba4f9d43e460efed7cbeb5
Dustin Sallings <dustin@spy.net>
2007-12-27 21:32:23 -0800

    Pulling auto-admin

--- vendor/plugins/auto_admin/lib/auto_admin.rb
+++
@@ -1,4 +0,0 @@
-
-require 'auto_admin_filter_sets'
-require 'auto_admin_configuration'
-
--- vendor/plugins/auto_admin/test/all_test.rb
+++
@@ -1,4 +0,0 @@
-%w(functional helper configuration label routing).each do |file|
-  require File.dirname(__FILE__) + "/#{file}_tests"
-end
-
