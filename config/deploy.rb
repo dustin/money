9127cafe8bdf8ae2f696e30827f1a53bfb8f567b
Dustin Sallings <dustin@spy.net>
2008-03-31 22:49:10 -0700

    Deploying with git.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "money"
-set :repository,  "http://hg.west.spy.net/hg/web/#{application}/"
+set :repository,  "git://github.com/dustin/#{application}.git"
+set :user, application
 set :runner, 'www'
 
 # If you aren't deploying to /u/apps/#{application} on the target
@@ -7,11 +8,23 @@ set :runner, 'www'
 # via the :deploy_to variable:
 set :deploy_to, "/data/web/rails/#{application}"
 
-set :scm, :mercurial
+set :scm, :git
+set :branch, "origin/master"
+set :deploy_via, :remote_cache
 
 role :app, "basket.west.spy.net"
 role :web, "basket.west.spy.net"
 role :db,  "basket.west.spy.net", :primary => true
+
+depend :remote, :command, "git"
+depend :remote, :gem, "SyslogLogger", ">= 1.4"
+depend :remote, :gem, "memcache-client", ">= 1.5"
+depend :remote, :gem, "sqlite3-ruby", "> 1.2"
+depend :remote, :gem, "god", ">= 0.6"
+# Older version of rails required for web services
+depend :remote, :gem, "actionpack", "= 1.13.6"
+depend :remote, :gem, "activerecord", "= 1.15.6"
+depend :remote, :gem, "activesupport", "= 1.4.4"
 
 desc "Starting and stopping via god."
 deploy.task :start do
