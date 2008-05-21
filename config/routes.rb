a1813ddc14cb9205d62b12b20b18d010812f8c0f
Dustin Sallings <dustin@spy.net>
2008-05-20 22:10:42 -0700

    Removed more API references.
    This will have to come back as some sort of restful thing *someday*.

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,6 @@ ActionController::Routing::Routes.draw do |map|
   # -- just remember to delete public/index.html.
   map.home '', :controller => "acct"
 
-  # Allow downloading Web Service WSDL as a file with an extension
-  # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
-
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
