06fc459e3cdfc0b95deb1372b884382cd9493414
Dustin Sallings <dustin@spy.net>
2008-05-22 22:31:09 -0700

    Trying out a bar chart.

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -12,4 +12,15 @@ module ReportHelper
     end
   end
 
+  def bar_chart(data, labels, opts = {})
+    if data.size > 1
+      width = opts.delete(:width) || 600
+      height = opts.delete(:height) || 200
+      tag :img, :alt => "pie",
+        :src => Gchart.bar(opts.merge({:data => data, :legend => labels,
+          :width => width, :height => height}))
+    end
+  end
+
+
 end
