e2b0e28dc1715a2df91bdd8d7724068c98f4ba83
Dustin Sallings <dustin@spy.net>
2008-05-22 22:15:36 -0700

    Pie chart for reports.

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,2 +1,15 @@
+require 'gchart'
+
 module ReportHelper
+
+  def pie_chart(data, labels, opts = {})
+    if data.size > 1
+      width = opts.delete(:width) || 400
+      height = opts.delete(:height) || 200
+      tag :img, :alt => "pie",
+        :src => Gchart.pie_3d(opts.merge({:data => data, :legend => labels,
+          :width => width, :height => height}))
+    end
+  end
+
 end
