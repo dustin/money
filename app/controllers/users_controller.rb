ca2b42295943bd637f3778b306e2a376b7bb5986
Dustin Sallings <dustin@spy.net>
2008-05-24 15:30:31 -0700

    Fixed password changing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,29 @@ class UsersController < ApplicationController
   # render new.rhtml
   def new
   end
+
+  def edit
+    title "Password change."
+  end
+
+  def update
+    user = User.authenticate current_user.login, params[:user][:current_password]
+    if user
+      user.crypted_password = nil
+      user.password = params[:user][:password]
+      user.password_confirmation = params[:user][:password_confirmation]
+      begin
+        user.save!
+        flash[:info] = 'Password changed.'
+        redirect_to home_path
+      rescue ActiveRecord::RecordInvalid => e
+        flash[:error] = e.message
+        redirect_to edit_user_path(current_user)
+      end
+    else
+      flash[:error] = 'Incorrect old password.'
+      redirect_to edit_user_path(current_user)
+    end
+  end
+
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -17,8 +17,47 @@ class UsersControllerTest < Test::Unit::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-  def test_nothing
-    assert true
+  def test_password_change_form
+    login_as :quentin
+    get :edit
+    assert_response :success
+  end
+
+  def test_password_change_bad_old_password
+    login_as :quentin
+    np = 'id28553'
+    put :update, :user => { :current_password => 'crap',
+      :password => np, :password_confirmation => np}
+    assert_redirected_to edit_user_path(users(:quentin))
+    assert_equal 'Incorrect old password.', flash[:error]
+  end
+
+  def test_password_change_new_pw_blank
+    login_as :quentin
+    np = ''
+    put :update, :user => { :current_password => 'test',
+      :password => np, :password_confirmation => np}
+    assert_redirected_to edit_user_path(users(:quentin))
+    assert_match /Password confirmation.*blank/, flash[:error]
+  end
+
+  def test_password_change_password_no_match
+    login_as :quentin
+    np = 'i2938g82'
+    put :update, :user => { :current_password => 'test',
+      :password => np, :password_confirmation => np + "x"}
+    assert_redirected_to edit_user_path(users(:quentin))
+    assert_match /doesn't match confirmation/, flash[:error]
+  end
+
+  def test_password_change_success
+    login_as :quentin
+    np = 'i2938g82'
+    put :update, :user => { :current_password => 'test',
+      :password => np, :password_confirmation => np}
+    assert_redirected_to home_path
+    assert_equal 'Password changed.', flash[:info]
+    assert User.authenticate('quentin', np)
   end
 
   protected
