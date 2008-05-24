c0f3c0dbc871189dac6dae47fcfe4eddbea0b3dd
Dustin Sallings <dustin@spy.net>
2008-05-24 16:47:31 -0700

    UI for creating a new account within a group.

--- app/controllers/money_accounts_controller.rb
+++ app/controllers/money_accounts_controller.rb
@@ -6,4 +6,17 @@ class MoneyAccountsController < ApplicationController
     @tasks=AllowanceTask.find_available current_user
   end
 
+  def new
+    @groups=current_user.groups
+  end
+
+  def create
+    acct = MoneyAccount.new params[:money_account]
+    acct.group = Group.find params[:money_account][:group_id]
+    acct.active = true
+    raise "Access denied" unless current_user.groups.include? acct.group
+    acct.save!
+    redirect_to home_path
+  end
+
 end
--- test/functional/money_accounts_controller_test.rb
+++ test/functional/money_accounts_controller_test.rb
@@ -28,4 +28,40 @@ class MoneyAccountsControllerTest < Test::Unit::TestCase
     assert_equal users(:quentin).groups, assigns['groups']
   end
 
+  def test_new_account_form
+    login_as :quentin
+    get :new
+    assert_response :success
+    assert_equal users(:quentin).groups, assigns(:groups)
+  end
+
+  def test_new_account
+    login_as :quentin
+    assert_difference 'MoneyAccount.count' do
+      post :create, :money_account => { :name => 'Test Acct',
+        :group_id => groups(:one).id }
+      assert_redirected_to home_path
+    end
+  end
+
+  def test_new_account_invalid_group
+    login_as :quentin
+    assert_no_difference 'MoneyAccount.count' do
+      assert_raises(ActiveRecord::RecordNotFound) do
+        post :create, :money_account => { :name => 'Test Acct',
+          :group_id => 2888533 }
+      end
+    end
+  end
+
+  def test_new_account_illegal_access
+    login_as :aaron
+    assert_no_difference 'MoneyAccount.count' do
+      assert_raises(RuntimeError) do
+        post :create, :money_account => { :name => 'Test Acct',
+          :group_id => groups(:two).id }
+      end
+    end
+  end
+
 end
